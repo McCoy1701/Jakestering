c83395f1ba7c54f4c181c88b0a17a011e36965e7
McCoy1701 <jkellum819@gmail.com>
2023-11-14 13:31:30 -0500

    fixed digitalWriteByte being reversed

--- src/main.c
+++ src/main.c
@@ -11,36 +11,36 @@ int main( int argc, char **argv )
 {
   setupIO();
 
-  for ( int i = 0; i < 8; i++ )
+  for ( int i = 0; i < 13; i++ )
   {
     pinMode( i , OUTPUT );
   }
 
-  digitalWriteByte( 0x80, 0, 7 );
+  digitalWriteByte( 0x80, 4, 11 );
   sleep( 1 );
   
-  digitalWriteByte( 0x40, 0, 7 );
+  digitalWriteByte( 0x40, 4, 11 );
   sleep( 1 );
   
-  digitalWriteByte( 0x20, 0, 7 );
+  digitalWriteByte( 0x20, 4, 11 );
   sleep( 1 );
   
-  digitalWriteByte( 0x10, 0, 7 );
+  digitalWriteByte( 0x10, 4, 11 );
   sleep( 1 );
   
-  digitalWriteByte( 0x08, 0, 7 );
+  digitalWriteByte( 0x08, 4, 11 );
   sleep( 1 );
   
-  digitalWriteByte( 0x04, 0, 7 );
+  digitalWriteByte( 0x04, 4, 11 );
   sleep( 1 );
   
-  digitalWriteByte( 0x02, 0, 7 );
+  digitalWriteByte( 0x02, 4, 11 );
   sleep( 1 );
   
-  digitalWriteByte( 0x01, 0, 7 );
+  digitalWriteByte( 0x01, 4, 11 );
   sleep( 1 );
 
-  digitalWriteByte( 0x00, 0, 7 );
+  digitalWriteByte( 0x00, 4, 11 );
   sleep( 1 );
 
 /*  keypad = initKeypad( 0, 1, 2, 3, 7, 6, 5, 4 );
